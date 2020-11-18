6962da247d74bca3d42bbd2e9882d6856f4979dc
Isabelly Oliveira Cunha <iolivecu@everis.com>
2020-11-18 00:16:42 -0300

    Exemplo de mensageria enviando e recebendo dados

---
+++ src/main/java/br/testemensageria/app/TestemensageriaApplication.java
@@ -0,0 +1,14 @@
+package br.testemensageria.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestemensageriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestemensageriaApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/br/testemensageria/testemensageria/TestemensageriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.testemensageria.testemensageria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestemensageriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
