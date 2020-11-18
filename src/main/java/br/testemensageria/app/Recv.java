674efcacf4263817749fa83e2f2ed8ee54b00876
Isabelly Oliveira Cunha <iolivecu@everis.com>
2020-11-18 09:22:06 -0300

    Código comentado para melhor entendimento

--- src/main/java/br/testemensageria/app/Recv.java
+++ src/main/java/br/testemensageria/app/Recv.java
@@ -9,18 +9,21 @@ public class Recv {
     private final static String QUEUE_NAME = "hello";
 
     public static void main(String[] argv) throws Exception {
+    	//criando conexao 
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
-
+        //declarando a queue que receberá a mensagem
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-
+        
+        //dizendo ao server para devolver a mensagem que está no queue
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
             System.out.println(" [x] Received '" + message + "'");
         };
+        //a variavel channel estabelece a conexao com o queue e devolve a mensagem
         channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
     }
 }
--- src/main/java/br/testemensageria/app/Send.java
+++ src/main/java/br/testemensageria/app/Send.java
@@ -11,13 +11,19 @@ public class Send {
     private final static String QUEUE_NAME = "hello";
 
     public static void main(String[] argv) throws Exception {
+    	//criando conexão com o server
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
+        //tentando a conexão
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
+        	//declaração da queue
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            //setando uma mensagem a ser publicada
             String message = "Hello World1!";
+            //aderindo a mensagem ao queue
             channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+            //mostrando que a mensagem foi enviada
             System.out.println(" [x] Sent '" + message + "'");
         }
     }
